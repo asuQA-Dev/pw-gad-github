fffa14004f985d804361b71d7f4773473d042c09
asuQA <asu.qa.engineer@gmail.com>
2025-06-06 19:08:21 +0200

    chore: eslint config with playwright

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,6 @@
+import eslintPluginPlaywright from 'eslint-plugin-playwright';
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+
 import js from '@eslint/js';
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
@@ -16,12 +19,30 @@ export default defineConfig([
     files: ['**/*.ts'],
     languageOptions: { globals: globals.node },
   },
-
   {
     rules: {
       'no-console': 'error',
     },
   },
-
+  {
+    rules: {
+      '@typescript-eslint/explicit-function-return-type': 'error',
+    },
+  },
+  eslintPluginPlaywright.configs['flat/recommended'],
+  {
+    rules: {
+      'playwright/no-nested-step': 'off',
+    },
+    settings: {
+      playwright: {
+        globalAliases: {
+          test: ['setup'],
+        },
+      },
+    },
+  },
   tseslint.configs.recommended,
+
+  eslintPluginPrettierRecommended,
 ]);
