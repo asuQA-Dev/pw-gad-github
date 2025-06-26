05602e0205a0ca750dbdfe22638e44b8897abd9d
asuQA <asu.qa.engineer@gmail.com>
2025-06-26 13:29:23 +0200

    chore: using alias for base page

--- src/pages/comment.page.ts
+++ src/pages/comment.page.ts
@@ -1,5 +1,5 @@
-import { BasePage } from './base.page';
 import { MainMenuComponents } from '@_src/components/main-menu.components';
+import { BasePage } from '@_src/pages/base.page';
 import { Locator, Page } from '@playwright/test';
 
 export class CommentPage extends BasePage {
