85c9825d3e94cc5876e3f6527f2b91e26b6f551e
asuQA <asu.qa.engineer@gmail.com>
2025-06-26 13:37:32 +0200

    chore: refactor config placement and naming

--- tests/setup/login.setup.ts
+++ tests/setup/login.setup.ts
@@ -4,7 +4,7 @@ import { WelcomePage } from '@_src/pages/welcome.page';
 import { testUser1 } from '@_src/test-data/user.data';
 import { expect, test as setup } from '@playwright/test';
 
-setup('Login with correct credentials', async ({ page }) => {
+setup('login adn save session', async ({ page }) => {
   // Arrange:
   const expectedTitle = 'Welcome';
   const loginPage = new LoginPage(page);
