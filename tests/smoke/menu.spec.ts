5be160a5afc2afd70ae880ba9f1efa79004905da
asuQA <asu.qa.engineer@gmail.com>
2025-07-24 17:22:39 +0200

    chore: wip adding new page fixtures

--- tests/smoke/menu.spec.ts
+++ tests/smoke/menu.spec.ts
@@ -1,34 +1,11 @@
-// import { ArticlesPage } from '@_src/pages/articles.page';
-import { ArticlesPage } from '@_src/pages/articles.page';
-import { CommentsPage } from '@_src/pages/comments.page';
-import { test as baseTest, expect } from '@playwright/test';
-
-interface Pages {
-  articlesPage: ArticlesPage;
-  commentsPage: CommentsPage;
-}
-
-const test = baseTest.extend<Pages>({
-  articlesPage: async ({ page }, use) => {
-    const articlesPage = new ArticlesPage(page);
-    await articlesPage.goto();
-    await use(articlesPage);
-  },
-  commentsPage: async ({ page }, use) => {
-    const commentsPage = new CommentsPage(page);
-    await commentsPage.goto();
-    await use(commentsPage);
-  },
-});
+import { expect, test } from '@_src/fixtures/merge.fixture';
 
 test.describe('Verify menu main button', () => {
   test('comments button navigates to comments page', { tag: '@GAD_R01_03' }, async ({ articlesPage }) => {
     // Arrange:
     const expectedTitle = 'Comments';
-    // const articlesPage = new ArticlesPage(page);
 
     // Act:
-    // await articlesPage.goto();
     const commentsPage = await articlesPage.mainMenuComponents.clickCommentsButton();
     const title = await commentsPage.getTitle();
 
@@ -39,10 +16,8 @@ test.describe('Verify menu main button', () => {
   test('article button navigates to comments page', { tag: '@GAD_R01_03' }, async ({ commentsPage }) => {
     // Arrange:
     const expectedTitle = 'Articles';
-    // const commentsPage = new CommentsPage(page);
 
     // Act:
-    // await commentsPage.goto();
     const articlesPage = await commentsPage.mainMenuComponents.clickArticlesButton();
     const title = await articlesPage.getTitle();
 
@@ -53,7 +28,6 @@ test.describe('Verify menu main button', () => {
   test('home page button navigates to main page', { tag: '@GAD_R01_03' }, async ({ articlesPage }) => {
     // Arrange:
     const expectedTitle = 'GAD';
-    // const articlesPage = new ArticlesPage(page);
 
     // Act:
     await articlesPage.goto();
