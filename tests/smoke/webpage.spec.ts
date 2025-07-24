64cea6d8dcc92bf948bcadfd82bdde64c8d1ee39
asuQA <asu.qa.engineer@gmail.com>
2025-07-24 17:52:40 +0200

    chore: own fixtures in smoke tests

--- tests/smoke/webpage.spec.ts
+++ tests/smoke/webpage.spec.ts
@@ -1,42 +1,27 @@
-import { ArticlesPage } from '@_src/pages/articles.page';
-import { CommentsPage } from '@_src/pages/comments.page';
-import { HomePage } from '@_src/pages/home.page';
-import { expect, test } from '@playwright/test';
+import { expect, test } from '@_src/fixtures/merge.fixture';
 
 test.describe('Verify service main page', () => {
-  test('home page title', { tag: '@GAD-R01-01' }, async ({ page }) => {
+  test('home page title', { tag: '@GAD-R01-01' }, async ({ homePage }) => {
     // Arrange:
     const expectedTitle = 'GAD';
-    const homePage = new HomePage(page);
-
-    // Act:
-    await homePage.goto();
 
     // Assert:
     const title = await homePage.getTitle();
     expect(title).toContain(expectedTitle);
   });
 
-  test('articles page title', { tag: '@GAD-R01-02' }, async ({ page }) => {
+  test('articles page title', { tag: '@GAD-R01-02' }, async ({ articlesPage }) => {
     // Arrange:
     const expectedTitle = 'Articles';
-    const articlesPage = new ArticlesPage(page);
-
-    // Act:
-    await articlesPage.goto();
 
     // Assert:
     const title = await articlesPage.getTitle();
     expect(title).toContain(expectedTitle);
   });
 
-  test('comments page title', { tag: '@GAD-R01-02' }, async ({ page }) => {
+  test('comments page title', { tag: '@GAD-R01-02' }, async ({ commentsPage }) => {
     // Arrange:
     const expectedTitle = 'Comments';
-    const commentsPage = new CommentsPage(page);
-
-    // Act:
-    await commentsPage.goto();
 
     // Assert:
     const title = await commentsPage.getTitle();
