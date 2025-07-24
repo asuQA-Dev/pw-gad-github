858d8135d5aa2b30449acc140d3bfff3af5cdb7d
asuQA <asu.qa.engineer@gmail.com>
2025-07-24 18:12:56 +0200

    chore: own fixtures in register tests

--- tests/integration/register.spec.ts
+++ tests/integration/register.spec.ts
@@ -1,47 +1,47 @@
 import { prepareRandomUser } from '@_src/factories/user.factory';
+import { expect, test } from '@_src/fixtures/merge.fixture';
 import { RegisterUserModel } from '@_src/models/user.model';
-import { RegisterPage } from '@_src/pages/register.page';
-import { expect, test } from '@playwright/test';
 
 test.describe('Verify register', () => {
-  let registerPage: RegisterPage;
   let registerUserData: RegisterUserModel;
 
-  test.beforeEach(async ({ page }) => {
-    registerPage = new RegisterPage(page);
+  test.beforeEach(async () => {
     registerUserData = prepareRandomUser();
-    await registerPage.goto();
   });
 
-  test('Register with correct data and login', { tag: '@GAD-R03-01, @GAD-R03-02, @GAD-R03-03' }, async () => {
-    // Arrange:
-    const expectedPopupMessage = 'User created';
-    const expectedTitleLogin = 'Login';
-    const expectedTitleWelcome = 'Welcome';
+  test(
+    'Register with correct data and login',
+    { tag: '@GAD-R03-01, @GAD-R03-02, @GAD-R03-03' },
+    async ({ registerPage }) => {
+      // Arrange:
+      const expectedPopupMessage = 'User created';
+      const expectedTitleLogin = 'Login';
+      const expectedTitleWelcome = 'Welcome';
 
-    // const loginPage = new LoginPage(page);
+      // const loginPage = new LoginPage(page);
 
-    const loginData = {
-      userEmail: registerUserData.userEmail,
-      userPassword: registerUserData.userPassword,
-    };
+      const loginData = {
+        userEmail: registerUserData.userEmail,
+        userPassword: registerUserData.userPassword,
+      };
 
-    // Act:
-    const loginPage = await registerPage.registerUser(registerUserData);
+      // Act:
+      const loginPage = await registerPage.registerUser(registerUserData);
 
-    // Assert:
-    await expect(registerPage.alertPopup).toHaveText(expectedPopupMessage);
-    await loginPage.waitForPageLoadToUrl();
+      // Assert:
+      await expect(registerPage.alertPopup).toHaveText(expectedPopupMessage);
+      await loginPage.waitForPageLoadToUrl();
 
-    const titleLogin = await loginPage.getTitle();
-    expect.soft(titleLogin).toContain(expectedTitleLogin);
+      const titleLogin = await loginPage.getTitle();
+      expect.soft(titleLogin).toContain(expectedTitleLogin);
 
-    const welcomePage = await loginPage.login(loginData);
-    const titleWelcome = await welcomePage.getTitle();
-    expect(titleWelcome).toContain(expectedTitleWelcome);
-  });
+      const welcomePage = await loginPage.login(loginData);
+      const titleWelcome = await welcomePage.getTitle();
+      expect(titleWelcome).toContain(expectedTitleWelcome);
+    },
+  );
 
-  test('Not register with incorrect data - non valid email', { tag: '@GAD-R03-04' }, async () => {
+  test('Not register with incorrect data - non valid email', { tag: '@GAD-R03-04' }, async ({ registerPage }) => {
     // Arrange:
     const expectedErrorText = 'Please provide a valid email address';
     registerUserData.userEmail = '#$%';
@@ -52,7 +52,7 @@ test.describe('Verify register', () => {
     // Assert:
     await expect(registerPage.emailErrorText).toHaveText(expectedErrorText);
   });
-  test('Not register with incorrect data - email not provided', { tag: '@GAD-R03-04' }, async () => {
+  test('Not register with incorrect data - email not provided', { tag: '@GAD-R03-04' }, async ({ registerPage }) => {
     // Arrange:
     const expectedEmailErrorText = 'This field is required';
 
